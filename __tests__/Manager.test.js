241f045d2dbcfc0fea8d43e105c0a0f374516fef
terahje <terahjequinn@gmail.com>
2020-10-03 22:01:36 -0700

    added manager.js and test to pass

--- __tests__/Manager.test.js
+++ __tests__/Manager.test.js
@@ -1,1 +1,21 @@
 const Manager = require('../lib/Manager');
+const Employee = require('../lib/Employee');
+
+test("Can set office number", () => {
+    const testOffice = 87;
+    
+    const manager = new Manager("Foo", 1, "email@email.com", testOffice);
+    expect(manager.officeNumber).toBe(testOffice)
+});
+
+test("getRole() should return Manager", () => {
+    const mRole = 'Manager';
+    const manager = new Manager("Foo", 1, "email@email.com", 87)
+    expect(manager.getRole()).toBe(mRole);
+});
+
+test("Can get office number", () => {
+    const testOffice = 87;
+    const manager = new Manager("Foo", 1, "email@email.com", testOffice)
+    expect(manager.getOfficeNumber()).toBe(testOffice);
+})
